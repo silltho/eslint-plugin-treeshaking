44605f0663243b3cde4de3255a7f87c6fa83d33b
Thomas Siller <siller.thomas@gmx.net>
2018-03-31 00:23:19 +0200

    add prettier

--- tests/lib/rules/no-namespace-imports.js
+++ tests/lib/rules/no-namespace-imports.js
@@ -1,43 +1,40 @@
 /**
- * @fileoverview  
+ * @fileoverview
  * @author Thomas Siller
  */
-"use strict";
+'use strict'
 
 //------------------------------------------------------------------------------
 // Requirements
 //------------------------------------------------------------------------------
 
-var rule = require("../../../lib/rules/no-namespace-imports"),
-
-    RuleTester = require("eslint").RuleTester;
+let rule = require('../../../lib/rules/no-namespace-imports'),
+  RuleTester = require('eslint').RuleTester
 
 RuleTester.setDefaultConfig({
-    parserOptions: {
-        ecmaVersion: 6,
-        sourceType: "module"
-    }
-});
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module'
+  }
+})
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-var ruleTester = new RuleTester();
-ruleTester.run("no-namespace-imports", rule, {
+let ruleTester = new RuleTester()
+ruleTester.run('no-namespace-imports', rule, {
+  valid: ["import $ from 'jquery';", "import { filter } from 'lodash/fp'"],
 
-    valid: [
-        "import $ from 'jquery';",
-        "import { filter } from 'lodash/fp'"
-    ],
-
-    invalid: [
+  invalid: [
+    {
+      code: "import * as lib from 'libary'",
+      errors: [
         {
-            code: "import * as lib from 'libary'",
-            errors: [{
-                message: "Prefer importing single functions over a full module",
-                type: "ImportDeclaration"
-            }]
+          message: 'Prefer importing single functions over a full module',
+          type: 'ImportDeclaration'
         }
-    ]
-});
+      ]
+    }
+  ]
+})
