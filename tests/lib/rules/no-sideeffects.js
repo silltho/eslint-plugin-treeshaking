2570f7384661bbbc6abf7378cf33d7738120e5ef
Thomas Siller <siller.thomas@gmx.net>
2018-05-09 09:32:18 +0200

    new rule no sideeffects

---
+++ tests/lib/rules/no-sideeffects.js
@@ -0,0 +1,66 @@
+/**
+ * @fileoverview
+ * @author Thomas Siller
+ */
+'use strict'
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+let rule = require('../../../lib/rules/no-sideeffects'),
+  RuleTester = require('eslint').RuleTester
+
+RuleTester.setDefaultConfig({
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module'
+  }
+})
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+let ruleTester = new RuleTester()
+ruleTester.run('no-namespace-imports', rule, {
+  valid: [
+    `
+    import * as lib1 from 'libary1'
+    import {func1} from 'libary2'
+    import Lib3 from 'libary3'
+    
+    export const export1 = 'export1'
+    export const exportFunc = () => 'exportFunc'
+    export default {
+      export1,
+      export2: 'export2'
+    }
+  `
+  ],
+
+  invalid: [
+    {
+      code: `
+        import * as lib1 from 'libary1'
+        import {func1} from 'libary2'
+        import Lib3 from 'libary3'
+        
+        console.log('sideeffect')
+        
+        export const export1 = 'export1'
+        export const exportFunc = () => 'exportFunc'
+        export default {
+          export1,
+          export2: 'export2'
+        }
+      `,
+      errors: [
+        {
+          message: 'sideeffects are not allowed',
+          type: 'ExpressionStatement'
+        }
+      ]
+    }
+  ]
+})
